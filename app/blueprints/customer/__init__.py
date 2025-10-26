7d77235a8765ede6a425e4db5eee03c931f7c85a
alhadwan <ali.hadwan@ymail.com>
2025-10-26 01:20:00 -0700

    remove comment

--- app/blueprints/customer/__init__.py
+++ app/blueprints/customer/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-customers_bp = Blueprint('customers_bp', __name__) # Blueprint for customer routes how? - and what is the __name__ for? The __name__ is the name of the current module, which is used by Flask to determine the location of templates and static files.
+customers_bp = Blueprint('customers_bp', __name__)
 
 from . import routes
